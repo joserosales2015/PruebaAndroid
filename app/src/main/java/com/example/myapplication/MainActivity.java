440da10875b46c8be3c7599e6b97fc8b0a57fd83
Jose Rosales <joserosales2015@gmail.com>
2020-03-13 00:05:11 -0500

    segundo envio

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lblsaludo = findViewById(R.id.lblSaludo);
-        lblsaludo.setText("pruebitas");
-    }
+        lblsaludo.setText("pruebitas agregado");
+}
     }
